4ff8d7ff6c662c778b114dd75d9b19c541050ab4
chonamdoo <psymarine@gmail.com>
2018-08-14 18:13:23 +0900

    그래들 파일 추가

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,55 @@
+object Dependencies{
+    val gradle = "com.android.tools.build:gradle:${Versions.gradle}"
+    val junit = "junit:junit:${Versions.junit}"
+    val espresso = "androidx.test.espresso:espresso-core:${Versions.espresso}"
+    val espressoContrib = "androidx.test.espresso:espresso-contrib:${Versions.espresso}"
+
+    val material = "com.google.android.material:material:${Versions.material}"
+    val appCompat = "androidx.appcompat:appcompat:${Versions.appcompat}"
+    val recyclerView = "androidx.recyclerview:recyclerview:${Versions.recyclerView}"
+    val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+
+    val annotation = "androidx.annotation:annotation:${Versions.annotation}"
+
+    val ktx = "androidx.core:core-ktx:${Versions.ktx}"
+
+    val glide = "com.github.bumptech.glide:glide:${Versions.glide}"
+    val glideCompiler = "com.github.bumptech.glide:compiler:${Versions.glide}"
+
+    val kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+    val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+
+    val coroutinesAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutinesAndroid}"
+
+    val rxKotlin = "io.reactivex.rxjava2:rxkotlin:${Versions.rxKotlin}"
+    val rxAndroid = "io.reactivex.rxjava2:rxandroid:${Versions.rxAndroid}"
+
+    val timber ="com.jakewharton.timber:timber:${Versions.timber}"
+
+    val daggerAndroid = "com.google.dagger:dagger-android:${Versions.dagger}"
+    val daggerSupport = "com.google.dagger:dagger-android-support:${Versions.dagger}"
+    val daggerCompiler = "com.google.dagger:dagger-compiler:${Versions.dagger}"
+    val daggerProcessor = "com.google.dagger:dagger-android-processor:${Versions.dagger}"
+
+    val lifecycle = "androidx.lifecycle:lifecycle-extensions:${Versions.lifeCycle}"
+
+    val room = "androidx.room:room-runtime:${Versions.room}"
+    val roomRxjava2 = "androidx.room:room-rxjava2:${Versions.roomRxjava2}"
+    val roomCompiler = "androidx.room:room-compiler:${Versions.roomCompiler}"
+
+    val okhttp = "com.squareup.okhttp3:okhttp:${Versions.okHttp}"
+    val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+    val retrofitConvertGson = "com.squareup.retrofit2:converter-gson:${Versions.retrofit}"
+    val retrofitAdapter = "com.squareup.retrofit2:adapter-rxjava2:${Versions.retrofit}"
+    val okhttpLogging = "com.squareup.okhttp3:logging-interceptor:${Versions.okhttpLogging}"
+    val stetho = "com.facebook.stetho:stetho:${Versions.stetho}"
+    val stethoOkhttp = "com.facebook.stetho:stetho-okhttp3:${Versions.stetho}"
+
+    val memoryLeakDebug = "com.squareup.leakcanary:leakcanary-android:${Versions.memoryLeak}"
+    val memoryLeakRelease = "com.squareup.leakcanary:leakcanary-android-no-op:${Versions.memoryLeak}"
+
+    val mockito = "org.mockito:mockito-core:${Versions.mockito}"
+    val mockitoAndroid = "org.mockito:mockito-android:${Versions.mockito}"
+    val mockWebServer = "com.squareup.okhttp3:mockwebserver:${Versions.mockWebServer}"
+
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,39 @@
+object Versions{
+    //App version
+    const val versionCode = 1
+    const val versionName = "1.0"
+
+    // SDK and Tools
+    const val compileSdk = 28
+    const val minSdk = 21
+    const val targetSdk = 28
+
+    // App dependencies
+    const val annotation = "1.0.0-beta01"
+    const val espresso = "3.1.0-alpha1"
+    const val gradle = "3.2.0-beta04"
+    const val junit = "4.12"
+    const val appcompat = "1.0.0-rc01"
+    const val constraintLayout = "2.0.0-alpha1"
+    const val kotlin = "1.2.60"
+    const val coroutinesAndroid = "0.24.0"
+    const val rxKotlin = "2.3.0"
+    const val rxAndroid = "2.0.2"
+    const val lifeCycle = "2.0.0-alpha1"
+    const val dagger ="2.16"
+    const val room = "2.0.0-alpha1"
+    const val roomRxjava2 ="2.0.0-alpha1"
+    const val roomCompiler = "2.0.0-alpha1"
+    const val retrofit = "2.4.0"
+    const val okhttpLogging = "3.11.0"
+    const val okHttp ="3.11.0"
+    const val timber = "4.7.1"
+    const val memoryLeak = "1.5.4"
+    const val glide = "4.7.1"
+    const val mockito = "2.7.22"
+    const val mockWebServer = "3.9.0"
+    const val stetho = "1.5.0"
+    const val ktx = "1.0.0-alpha1"
+    const val material = "1.0.0-alpha1"
+    const val recyclerView = "1.0.0-alpha1"
+}
